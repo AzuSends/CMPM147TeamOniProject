f09ae3a0c40e5b29adbc522b6c801df0c8c60080
ljcui1 <yangyangjiaocui@gmail.com>
2025-05-15 16:00:23 -0700

    adding dock

--- js/sketch.js
+++ js/sketch.js
@@ -10,6 +10,7 @@ let freq = 0.05;
 
 
 function setup() {
+    noSmooth();
     //createCanvas(800,400)
 
     //setup() from corvuscorae reflected clouds project
@@ -19,9 +20,35 @@ function setup() {
     regenerate();
 }
 
+function preload() {
+    /*dock1 = loadImage('../assets/dockSplit 147_1.png');
+    dock2 = loadImage('../assets/dockSplit 147_2.png');
+    dock3 = loadImage('../assets/dockSplit 147_3.png');
+    dock = loadImage('../assets/dock 147.png');*/
+    dockEnd = loadImage('../assets/dockEnd_x5.png');
+    dockMid = loadImage('../assets/dockMid_x5.png');
+    dockMidLeg = loadImage('../assets/dockMidLeg_x5.png');
+    dockLeg = loadImage('../assets/dockLeg_x5.png');
+}
+
 function draw() {
     clear();
     perlinSky();
+    /*dock.resize(240, 160);
+    image(dock, 0, h / 3 + 10);*/
+    //place dock tiles
+    image(dockMidLeg, 0, h - 160);
+    image(dockMid, 80, h - 160);
+    image(dockMid, 160, h - 160);
+    image(dockMidLeg, 240, h - 160);
+    image(dockMid, 320, h - 160);
+    image(dockMid, 400, h - 160);
+    image(dockEnd, 480, h - 160);
+
+    image(dockLeg, 0, h - 80);
+    image(dockLeg, 240, h - 80);
+    image(dockLeg, 480, h - 80);
+
 }
 
 //perlinSky() and regenerate() functions from corvuscorae reflected clouds project
