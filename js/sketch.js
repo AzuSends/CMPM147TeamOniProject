02332ef54647fe6d4b1fc987f21dc79623b01c16
ljcui1 <yangyangjiaocui@gmail.com>
2025-05-14 22:05:41 -0700

    attempting to hide reflection point, now stretched to the left for some reason

--- js/sketch.js
+++ js/sketch.js
@@ -33,10 +33,10 @@ function perlinSky() {
     let scale = 0.09;
     noiseSeed(skySeed);
   
-    for (let y = h; y < h * 1.5; y++) {  // shifted up to reflect at horizon
+    for (let y = 0; y < h / 2; y++) {  // shifted up to reflect at horizon
         for (let x = 0; x < w; x++) {
             // modify scale along y-axis, squishing it as y gets larger
-            let mod = map(y, 0, h, 10, 1);
+            let mod = map(y, 0, h / 2, 10, 1);
             let squish = scale / mod;    
             
             let nx = squish * x;
@@ -47,7 +47,7 @@ function perlinSky() {
             let strokeColor = color(100, 149, 237);
             if(c > 200){ strokeColor = "skyblue"; }
             stroke(strokeColor);
-            point(x, y - h/2);
+            point(x, h - y - 1);
         }
     }
 }
