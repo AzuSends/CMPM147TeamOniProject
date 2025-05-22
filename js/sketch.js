0cc6b4535fc5f9b1607cec9027ee058e92238647
Benjamin Silver <besilver@ucsc.edu>
2025-05-21 18:41:14 -0700

    Change image file paths for github compatability

--- js/sketch.js
+++ js/sketch.js
@@ -44,10 +44,10 @@ function setup() {
 }
 
 function preload() {
-    dockEnd = loadImage('../assets/dockEnd_x5.png');
-    dockMid = loadImage('../assets/dockMid_x5.png');
-    dockMidLeg = loadImage('../assets/dockMidLeg_x5.png');
-    dockLeg = loadImage('../assets/dockLeg_x5.png');
+    dockEnd = loadImage('/assets/dockEnd_x5.png');
+    dockMid = loadImage('/assets/dockMid_x5.png');
+    dockMidLeg = loadImage('/assets/dockMidLeg_x5.png');
+    dockLeg = loadImage('/assets/dockLeg_x5.png');
 
     //player img
     player = loadImage('../assets/fish_x5.png');
